7ecd19c678fbfbf268e848b354a7184d551443f9
fsalva157 <xxavierargentino@hotmail.com>
2023-03-01 20:47:56 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,18 @@
+from servicio import *
+
+archivo1 = FileManager('db.json')
+#archivo1.create({"Guiso": "Receta de Guiso","Sopa": "Receta de Sopa","Empanada": "Receta de Empanada"})
+print(archivo1.getAll())
+#print(archivo1.getOne("Guiso majo"))
+
+#archivo1.borrar("Sopa")
+#datos = archivo1.leer()
+#print(archivo1.addOne("Pizza", 12545))
+
+# if archivo1.addOne("Sopa Crema","sopa crema de arvejas"):
+#     print("EXITO")
+# else:
+#     print("YA EXISTE LA RECETA")    
+
+#print(archivo1.deleteOne("Pizza"))
+#print(archivo1.updateOne("Sopa","sopa de cabello de angel"))
---
+++ servicio.py
@@ -0,0 +1,93 @@
+import json
+class FileManager:
+    base_url = 'data/'        
+
+    def __init__(self, archivo: str):
+        self.__archivo = archivo        
+    
+
+    def create(self, info:dict):
+        try:
+            with open(FileManager.base_url + self.__archivo, "w") as write_file:
+                json.dump(info, write_file, indent=4)
+                write_file.close()
+        except IOError as e:
+            raise Exception(f"Error de Entrada/Salida al crear el archivo: {self.__archivo}, error: {e}")
+        except Exception as e:
+            raise Exception(f"Error Inesperado al crear el archivo: {self.__archivo}, error: {e}")
+
+    def getAll(self):
+        try: 
+            with open(FileManager.base_url+self.__archivo, "r") as read_file:
+                data = json.load(read_file) 
+            # return data
+        except FileNotFoundError as e:
+            raise Exception(f"No se encontro el archivo: {self.__archivo}")
+        except ValueError as e:
+            raise Exception(f"Error al leer el archivo: {self.__archivo}, error: {e}")
+        except Exception as e:
+            raise Exception(f"Error Inesperado al leer el archivo: {self.__archivo}, error: {e}")
+        else:
+            return data
+        
+    def getOne(self, key: str):
+        try:            
+            data = self.getAll()
+            if(key not in data):
+                raise Exception(f"La clave consultada no existe en los datos")
+            obj = data[key]
+            # return obj
+        except ValueError as e:
+            raise Exception(f"Error al leer el archivo: {self.__archivo}, error: {e}")
+        except Exception as e:
+            raise Exception(f"Error Inesperado al leer el archivo: {self.__archivo}, error: {e}")
+        else:
+            return obj
+
+    
+    def addOne(self, clave: str, valor) -> str: 
+        try:
+            data = self.getAll()
+            if(clave in  data):
+                raise Exception(f"el objeto que intenta agregar ya existe!")
+            else:
+                data[clave] = valor
+                with open(FileManager.base_url+self.__archivo,"w") as writable_file:
+                    json.dump(data, writable_file)
+                return "Operación de agregar datos Exitosa!!"
+        except FileNotFoundError as e:
+            raise Exception(f"No se encontro el archivo: {self.__archivo}")
+        except PermissionError as e:
+            raise Exception(f"El archivo {self.__archivo} no tiene permisos de escritura.")
+        except OSError as e:
+            raise Exception(f"Error al abrir o cerrar el archivo {self.__archivo}. Error: {e}")
+        except json.JSONDecodeError as e:
+            raise Exception(f"El archivo {self.__archivo} no se pudo decodificar correctamente. Error: {e}")
+        except ValueError as e:
+            raise Exception(f"Se intentó agregar un valor no válido al archivo JSON {self.__archivo}. Error: {e}")
+        except Exception as e:
+            raise Exception(f"Error Inesperado al escribir el archivo: {self.__archivo}, error: {e}")
+    
+    def deleteOne(self, llave: str)->str:
+        try:
+            info = self.getAll()
+            if(llave in info):
+                del info[llave]
+                self.create(info)
+                return f"El objeto {llave} ha sido eliminado Exitosamente!"      
+            else:
+                raise Exception(f"El objeto con clave {llave} no existe en los datos")
+        except IOError as e:
+            raise Exception(f"Error al intentar leer o escribir el archivo: {self.__archivo}, error: {e}") 
+        except Exception as e:
+            raise Exception(f"Error Inesperado al eliminar dato en el archivo: {self.__archivo}, error: {e}")
+        
+    def updateOne(self, key, value):
+        data = self.getAll()
+        if(key not in data):
+            return False
+        else:
+            data[key]=value
+            self.create(data)
+            return True
+        
